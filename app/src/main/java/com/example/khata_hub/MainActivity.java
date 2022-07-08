aa00d4beef17f198e7a693a7d3e0a734d727eeb3
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-07-08 08:34:12 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/khata_hub/MainActivity.java
+++ app/src/main/java/com/example/khata_hub/MainActivity.java
@@ -51,14 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 amount.setText(""+value_addon);
 
             }
-             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Money Have been Deducted", Toast.LENGTH_SHORT).show();
-                String s = enter_amt.getText().toString().trim();
-                int value_dedc = Integer.parseInt(s);
-                int value_addon = value_dedc - value_add ;;
-                amount.setText(""+value_addon);
-
-            }
+             p
         });
     }
 }
